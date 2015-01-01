1800d005da7c6a29d3f74ed71f8568b74b1a71d4
anishnath <anish2good@yahoo.co.in>
2015-01-01 09:52:27 +0000

    Aded he Cookbooks files

---
+++ cookbooks/browserapp/metadata.rb
@@ -0,0 +1,9 @@
+name             'browserapp'
+maintainer       'DevFactory'
+maintainer_email 'an25081981@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures browserapp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "google-chrome"
